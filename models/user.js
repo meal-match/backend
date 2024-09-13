1e45dd7c88995b5a33ef4f998454a78d4fe3e873
Sam Gaines <samuelmgaines@gmail.com>
2024-09-13 18:25:34 +0100

    Add required firstName and lastName fields to user

--- models/user.js
+++ models/user.js
@@ -15,6 +15,14 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true,
         minlength: 8
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
     }
 })
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,10 +6,10 @@ app.use(express.json())
 
 // Sign up route
 app.post('/signup', async (req, res) => {
-    const { email, password } = req.body
+    const { email, password, firstName, lastName } = req.body
 
     try {
-        await User.create({ email, password })
+        await User.create({ email, password, firstName, lastName })
         res.status(201).send('User created')
     } catch (err) {
         res.status(500).send('Internal server error:\n' + err)
