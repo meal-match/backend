17a122b27ea98442fef811c1b53771fe1aac995c
Sam Gaines <samuelmgaines@gmail.com>
2024-09-13 07:45:32 +0100

    Add very basic authentication with connection to database

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const bcrypt = require('bcrypt')
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^.+@(?:.+\.)?ua\.edu$/, 'Email must be a valid ua.edu address']
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    }
+})
+
+// Hash the password before saving the user document
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next()
+    } // Only hash if password was modified
+
+    try {
+        const salt = await bcrypt.genSalt(10) // Generate salt
+        this.password = await bcrypt.hash(this.password, salt) // Hash password with salt
+        next()
+    } catch (err) {
+        next(err)
+    }
+})
+
+// Method to compare hashed password with the provided password
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+    try {
+        return await bcrypt.compare(candidatePassword, this.password) // Compare passwords
+    } catch (err) {
+        throw new Error(err)
+    }
+}
+
+module.exports = mongoose.model('User', UserSchema)
---
+++ routes/authRoutes.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const User = require('../models/user')
+
+const app = express()
+app.use(express.json())
+
+// Sign up route
+app.post('/signup', async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        await User.create({ email, password })
+        res.status(201).send('User created')
+    } catch (err) {
+        res.status(500).send('Internal server error:\n' + err)
+    }
+})
+
+// Login route
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const user = await User.findOne({ email })
+
+        if (!user) {
+            return res.status(401).send('Unauthorized')
+        }
+
+        const isMatch = await user.comparePassword(password)
+
+        if (isMatch) {
+            res.status(200).send('Login successful')
+        } else {
+            res.status(401).send('Unauthorized')
+        }
+    } catch (err) {
+        res.status(500).send('Internal server error:\n' + err)
+    }
+})
+
+module.exports = app
