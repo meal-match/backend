8459d21c5a47f339b54dbcb2e446a38143d23271
Sam Gaines <samuelmgaines@gmail.com>
2024-09-14 22:29:01 +0100

    Return user profile information. Configure vercel

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const User = require('../models/user')
 
 const app = express()
 
 const isAuthenticated = (req, res, next) => {
-    if (req.session.userId) {
+    if (req.session?.userId) {
         next()
     } else {
         res.status(401).send('Unauthorized')
@@ -11,8 +12,19 @@ const isAuthenticated = (req, res, next) => {
 }
 
 // Profile route
-app.get('/', isAuthenticated, (req, res) => {
-    res.send('This is your profile')
+app.get('/', isAuthenticated, async (req, res) => {
+    try {
+        const user = await User.findById(
+            req.session.userId,
+            'firstName lastName email'
+        )
+        if (!user) {
+            return res.status(404).send('User not found')
+        }
+        res.status(200).json(user) // Return user profile data
+    } catch (err) {
+        res.status(500).send('Internal server error')
+    }
 })
 
 module.exports = app
