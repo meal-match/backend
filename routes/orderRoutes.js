d1a5c7c8f87f127392aa22b1fba3f47fdd213a7e
malachi-crain <malachicrain@gmail.com>
2025-01-11 22:24:33 -0600

    Minor PR fixes

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -249,7 +249,9 @@ app.patch('/:id/unclaim', isAuthenticated, async (req, res) => {
 
 app.patch('/:id/confirm', isAuthenticated, async (req, res) => {
     const { id } = req.params
-    const { readyTime } = req.query
+    // TODO: we also need to receive and save the receipt
+    const { readyTime } = req.body
+
     try {
         const order = await Order.findById(id)
         if (!order) {
@@ -257,6 +259,7 @@ app.patch('/:id/confirm', isAuthenticated, async (req, res) => {
                 message: 'Order not found'
             })
         }
+
         const user = await User.findById(req.session.userId, 'openOrders')
         if (!user) {
             return res.status(404).json({
@@ -264,26 +267,33 @@ app.patch('/:id/confirm', isAuthenticated, async (req, res) => {
                 message: 'User not found'
             })
         }
+
         if (order.status !== 'Claimed') {
             return res.status(400).json({
                 message:
                     'Order is not claimed and can therefore not be confirmed'
             })
         }
+
         if (order.seller.toString() !== req.session.userId) {
             return res.status(401).json({
                 message: 'Unauthorized'
             })
         }
-        if (!/^(0?[1-9]|1[0-2]):[0-5][0-9]\s?[APap][Mm]$/.test(readyTime)) {
+
+        if (!/^(0?[1-9]|1[0-2]):[0-5]\d\s?[APap][Mm]$/.test(readyTime)) {
             return res.status(400).json({
                 message: 'Invalid ready time format'
             })
         }
+
         order.status = 'Confirmed'
         order.readyTime = readyTime
         order.confirmationTime = new Date()
         await order.save()
+
+        // TODO: send push notification to buyer
+
         res.status(200).json({
             message: 'Order confirmed successfully'
         })
