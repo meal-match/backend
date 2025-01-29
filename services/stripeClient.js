3ad52dff5aac9c16b8a5099b9d8fb568a3b3a8d6
TheGoldenPatrik1 <55322484+TheGoldenPatrik1@users.noreply.github.com>
2025-01-29 17:34:56 -0600

    Payout methods (#22)
    - Abstract `fetchPaymentMethods` and `fetchPayoutMethods` into the
    `stripeClient.js` file
    - Allow the deletion of payment methods using the frontend Stripe UI
    - Upgrade dependencies to address security vulnerabilities
    - Add the following routes for `/payout`:
       - `GET /setup-intent`
       - `DELETE /setup-intent`
       - `PATCH /default-method/:id`
       - `DELETE /:id`

--- services/stripeClient.js
+++ services/stripeClient.js
@@ -5,4 +5,46 @@ dotenv.config()
 
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY)
 
+stripe.fetchPaymentMethods = async (user) => {
+    const customers = await stripe.customers.list({
+        email: user.email,
+        limit: 1
+    })
+    if (!customers.data.length) {
+        return []
+    }
+
+    const paymentMethods = await stripe.paymentMethods.list({
+        customer: customers.data[0].id,
+        type: 'card'
+    })
+    const defaultPaymentMethod = user.paymentMethods.find((pm) => pm.default)
+    for (const method of paymentMethods.data) {
+        method.default = method.id === defaultPaymentMethod?.id
+    }
+
+    return paymentMethods.data
+}
+
+stripe.fetchPayoutMethods = async (user) => {
+    const customers = await stripe.customers.list({
+        email: user.email,
+        limit: 1
+    })
+    if (!customers.data.length) {
+        return []
+    }
+
+    const payoutMethods = await stripe.paymentMethods.list({
+        customer: customers.data[0].id,
+        type: 'us_bank_account'
+    })
+    const defaultPayoutMethod = user.payoutMethods.find((pm) => pm.default)
+    for (const method of payoutMethods.data) {
+        method.default = method.id === defaultPayoutMethod?.id
+    }
+
+    return payoutMethods.data
+}
+
 module.exports = stripe
